a16a8b0fdaa38e581009f3bd750030020ece03d5
Kaio Masaru Nakazono <kmn1405.cg@gmail.com>
2022-02-26 16:21:38 -0300

    add url root

--- wisdompets/urls.py
+++ wisdompets/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('adoptions.urls')),
     path('adoptions/', include('adoptions.urls'))
 ]
